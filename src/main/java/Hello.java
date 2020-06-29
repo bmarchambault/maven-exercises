3b89f5d940b5202ab405560f515172f2d3ee5201
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-29 16:12:38 -0500

    commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //when you get version not supported, copy and paste build override in the pom.xml file. then click icon to reload or right click, maven, reload
+    }
+}
+
